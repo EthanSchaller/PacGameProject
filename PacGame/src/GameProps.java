5a4a96994434a99fae92953c96216afbd618e845
EthanSchaller <91805044+EthanSchaller@users.noreply.github.com>
2021-10-04 10:42:34 -0300

    Delete PacGame directory

--- PacGame/src/GameProps.java
+++
@@ -1,6 +0,0 @@
-
-public class GameProps {
-	public static final int SCREEN_WIDTH = 1000;
-	public static final int SCREEN_HEIGHT = 1000;
-	public static final int CHAR_STEP = 15;
-}
--- PacGame/src/Pacman.java
+++
@@ -1,4 +0,0 @@
-
-public class Pacman extends Sprite{
-	public Pacman() {super(50, 50, "PacR.gif");}
-}
